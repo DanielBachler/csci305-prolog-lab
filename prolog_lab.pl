3774652fba4f23fb6af36ae3dacfea4c0834e3c8
Dan <daniel.bachler@comcast.net>
2018-04-16 10:46:57 -0600

    Working on starting predicates
    Got all of the starting predicates written, working on testing and fixing still.

--- prolog_lab.pl
+++ prolog_lab.pl
@@ -1,41 +1,92 @@
 %Daniel Bachler
 %CSCI 305 Prolog Lab 2
 
+%The mother predicate, works
 mother(M,C) :-
     parent(M,C),
     female(M).
 
+%Father predicate, works
 father(F,C) :-
     parent(F,C),
     male(F).
 
-%not sure what second rule is
+%married predicate, works
 spouse(M,F) :-
     married(M,F).
 
-%Again not sure what second rule is
-child(P,C) :-
+%Finds the parent of the child, works
+child(C, P) :-
     parent(P,C).
 
+%Given a parent, finds the son(s), works
 son(P,S) :-
     parent(P,S),
     male(S).
 
+%Given parent, finds daughters, works
 daughter(P,D) :-
     parent(P,D),
     female(D).
 
+%Given a person, finds their siblings, works
 sibling(S1,S2) :-
-    parent(P1,S1),
-    parent(P2,S2),
-    =(P1,P2).
+    mother(M, S1), father(F, S1),
+    mother(M, S2), father(F, S2),
+    not(S1 = S2).
 
+%given a person finds their brother(s), works
 brother(S,B) :-
     sibling(S,B),
     male(B).
 
+%Given a person, finds their sisters, works
 sister(Sibling, S) :-
     sibling(Sibling, S),
     female(S).
+
+%Blood uncle, works
+uncle(U, N) :-
+    parent(X, N),
+    sibling(U,X).
+
+%marriage uncle
+uncle(U, N) :-
+    parent(X, N),
+    sibling(X, S),
+    married(U, S).
+
+%blood aunt
+aunt(A, N) :-
+    parent(P, N),
+    sibling(A, P).
+
+%marriage aunt
+aunt(A, N) :-
+    parent(P, N),
+    sibling(P, S),
+    married(S, A).
+
+%grandparent
+grandparent(GP, GC) :-
+    parent(P, GC),
+    parent(GP, P).
+
+%grandfather
+grandfather(GF, GC) :-
+    grandparent(GF, GC),
+    male(GF).
+
+%grandmother
+grandmother(GM, GC) :-
+    grandparent(GM, GC),
+    female(GM).
+
+%grandchild
+grandchild(GC, GP) :-
+    parent(P, GC),
+    parent(GP, P).
+
+
 
 
